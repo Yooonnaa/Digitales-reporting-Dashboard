f1c0df657c3dd5781524af068fd76692b389707e
Jakob Höynck <Jakob@hoeynck.net>
2025-07-22 15:32:34 +0200

    Add CSV export and import functionality for transactions

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import sys
 from templates import transaktionen_file
 from templates.transaktionen_file import neue_transaktion_erfassen
+from templates.transaktionen_file import transaktionen_aus_csv_laden
+from templates.transaktionen_file import transaktionen_als_csv_speichern
 
-#Hauptliste
-transaktionen = []
+transaktionen = transaktionen_aus_csv_laden()
+
+# Start-Menü
 def startpoint():
     while True:
         print("Was möchtest du tun?")
+        # Auswahlmöglichkeiten
         print("[1] Neue Einnahmen hinzufügen")
         print("[2] Neue Ausgabe hinzufügen")
         print("[0] Beenden")
@@ -15,12 +19,12 @@ def startpoint():
 
         if auswahl == "1":
             transaktionen.append(neue_transaktion_erfassen("Einnahme"))
+            print("Einnahme Erfasst!")
         elif auswahl == "2":
             transaktionen.append(neue_transaktion_erfassen("Ausgabe"))
+            print("Ausgabe Erfasst!")
         elif auswahl == "0":
-            print("\nErfasste Transaktionen:")
-            for t in transaktionen:
-                print(t)
+            transaktionen_als_csv_speichern(transaktionen)
             print("Programm Beendet.")
             break
         else:
--- app/templates/transaktionen_file.py
+++ app/templates/transaktionen_file.py
@@ -1,6 +1,7 @@
+import csv
 import datetime
 
-
+# Transaktionsvorlage
 def neue_transaktion_erfassen(typ):
     date = datetime.datetime.now()
     transaktion= {
@@ -12,5 +13,30 @@ def neue_transaktion_erfassen(typ):
     }
     return transaktion
 
+
+# Transaktion als CSV Speichern
+def transaktionen_als_csv_speichern(transaktionen, dateiname="transaktionen.csv"):
+    if not transaktionen:
+        print("Keine Transaktionen zum Speichern")
+        return
+
+    feldnamen = transaktionen[0].keys()
+
+    with open(dateiname, "w", newline="") as csv_datei:
+        writer = csv.DictWriter(csv_datei, fieldnames=feldnamen)
+        writer.writeheader()
+        writer.writerows(transaktionen)
+
+    print(f"Transaktionen wurden in {dateiname} gespeichert.")
+
+# Transaktionen als CSV Laden
+def transaktionen_aus_csv_laden(dateiname="transaktionen.csv"):
+    try:
+        with open(dateiname, "r") as csv_datei:
+            reader = csv.DictReader(csv_datei)
+            return list(reader)
+    except FileNotFoundError:
+        print(f"Datei {dateiname} nicht gefunden. Es werden keine Transaktionen geladen")
+        return[]
 
 
